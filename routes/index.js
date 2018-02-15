e429736ea6e63f8bc035491e143f495a8d698843
jason ho <ho.jason.main@gmail.com>
2018-02-14 19:02:13 -0800

    wizard of oz login pushes username into users in data.json

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,32 @@
-
+var data = require("../data.json")
 /*
  * GET home page.
  */
 
+function pushUnique(user){
+	//for(var item in data.users) {
+	if(JSON.stringify(data.users).indexOf(user.username) === -1) {
+		return data.users.push(user);
+	};
+	//};
+}
+
 exports.view = function(req, res){
-  res.render('index');
+	var user = {
+		"username":"Guest",
+	};
+
+	pushUnique(user);
+
+	res.render('index',user);
 };
+
+exports.viewName = function(req, res){
+	var user = {
+		"username":req.query.username,
+	};
+
+	pushUnique(user);
+
+	res.render('index',user);
+}
