e6ecb5985d9fcfbf0dbc129b9c458b86aae24a4d
Yi Cai <yic305@ucsd.edu>
2018-02-15 19:23:05 -0800

    made changes to suggestions

--- routes/suggestions.js
+++ routes/suggestions.js
@@ -1,4 +1,6 @@
 //const { newElement, myStepsArray } = require("./mainPage");
+var dataSteps = require("../steps.json")
+var dataSugg = require("../suggestions.json")
 
 /*
  * GET function1 page.
@@ -7,6 +9,21 @@
 exports.viewSuggestions = function(req, res){
   res.render('suggestions');
 };
+
+var givenSugg;
+
+function giveSugg(){
+	for (var item in dataSteps.codeRecs){
+		var stepWord = JSON.stringify(dataSteps.codeRecs[item]);
+		for (var item2 in dataSugg.sugg){
+			var recWord = JSON.stringify(dataSugg.sugg.english[item2]);
+			if (stepWord.includes(recWord) == true){
+				givenSugg = JSON.stringify(dataSugg.sugg.suggestions[item2]);
+			}
+
+		}
+	}
+}
 
 //console.log("");
 
