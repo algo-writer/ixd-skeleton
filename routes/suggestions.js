b6a74b0235d50dc859cc87fc263b84d984c530af
Maya Bello <mbello@ucsd.edu>
2018-02-22 21:43:23 -0800

    Styling done for suggestions

--- routes/suggestions.js
+++ routes/suggestions.js
@@ -14,22 +14,4 @@ exports.viewSuggestions = function(req, res){
 
 
 
-//var dataString = localStorage.getItem("main");
-
-
-// function giveSugg(){
-// 	for (var item in dataSteps.codeRecs){
-// 		var stepWord = JSON.stringify(dataSteps.codeRecs[item]);
-// 		for (var item2 in dataSugg.sugg){
-// 			var recWord = JSON.stringify(dataSugg.sugg.english[item2]);
-// 			if (stepWord.includes(recWord) == true){
-// 				givenSugg = JSON.stringify(dataSugg.sugg.suggestions[item2]);
-// 			}
-
-// 		}
-// 	}
-// }
-
-
-
 
