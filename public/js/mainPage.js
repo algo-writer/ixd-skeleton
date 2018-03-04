e5ee8a58a4ad20abad002ac8d505bf8c39ae4202
jason ho <ho.jason.main@gmail.com>
2018-03-04 13:03:32 -0800

    google analytics id script added

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -12,40 +12,40 @@ window.onload = function(){
 function closeButtons() {
 
 
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var button = document.createElement("BUTTON");
-  button.className="glyphicon glyphicon-remove";
-  span.className = "close";
-  span.contentEditable=false;
-  span.appendChild(button);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-    console.log(div.id);
-    deleteElement(div.id.substring(4));
+  var myNodelist = document.getElementsByTagName("LI");
+  var i;
+  for (i = 0; i < myNodelist.length; i++) {
+    var span = document.createElement("SPAN");
+    var button = document.createElement("BUTTON");
+    button.className="glyphicon glyphicon-remove";
+    span.className = "close";
+    span.contentEditable=false;
+    span.appendChild(button);
+    myNodelist[i].appendChild(span);
   }
-}
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
+  // Click on a close button to hide the current list item
+  var close = document.getElementsByClassName("close");
+  var i;
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+      console.log(div.id);
+      deleteElement(div.id.substring(4));
+    }
   }
-}, false);
 
-var myStepsArray = [];
-var storageArray = [];
+  // Add a "checked" symbol when clicking on a list item
+  var list = document.querySelector('ul');
+  list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+      ev.target.classList.toggle('checked');
+    }
+  }, false);
+
+  var myStepsArray = [];
+  var storageArray = [];
 }
 
 function appendToStorage(){
