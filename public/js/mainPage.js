e616b3121f1ac9bb129fd7dcd1991fd3f72df9a9
jason ho <ho.jason.main@gmail.com>
2018-03-04 02:43:55 -0800

    sloppy edit x delete v2

--- public/js/mainPage.js
+++ public/js/mainPage.js
@@ -1,3 +1,5 @@
+var disEdit=[];
+
 // Create a "close" button and append it to each list item
 window.onload = function(){
   console.log(sessionStorage);
@@ -29,6 +31,7 @@ for (i = 0; i < close.length; i++) {
   close[i].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
+    console.log(div.id);
     deleteElement(div.id.substring(4));
   }
 }
@@ -48,21 +51,34 @@ var storageArray = [];
 function appendToStorage(){
   console.log(document.getElementById("myInput").value);
   sessionStorage.setItem("key" + sessionStorage.length, document.getElementById("myInput").value);
-  console.log(sessionStorage);
+  disEdit.push(document.getElementById("myInput").value);
   
 }
 
 function deleteElement(num){
   //sessionStorage.removeItem("key" + num);
-  console.log("key" + num);
-  console.log("key" + (Number(num)+1));
+
+  for(var i = 0; i < sessionStorage.length;i++) {
+    disEdit[i]=$("#edit"+i).text();
+  }
+  disEdit.splice(num,1);
+  console.log(disEdit);
   for (var i = Number(num); i < sessionStorage.length; i++) {
-    sessionStorage.setItem("key" + i, sessionStorage.getItem("key" + (i+1)));
+    sessionStorage.setItem("key" + i, disEdit[i]);
   }
   sessionStorage.removeItem("key" + (sessionStorage.length-1));
-  $("#edit" + num).text(sessionStorage.getItem("key"+(num)));
+
+  for (var i = 0; i < disEdit.length;i++) {
+    $("#edit"+i).text(disEdit[i]);
+  }
+
   console.log(sessionStorage);
   // for (var j = 0; j < sessionStorage.length; j++) {
   //   $("#edit" + j).text(sessionStorage.getItem("key"+j));
   // }
+  $('#blah').empty();
+  for (var i = 0; i < sessionStorage.length; i++) {
+    $('#blah').append('<li contenteditable=true class="closeable" id="edit'+i+'">' + sessionStorage.getItem("key"+i) + '</li>');
+  }
+  closeButtons();
 }
